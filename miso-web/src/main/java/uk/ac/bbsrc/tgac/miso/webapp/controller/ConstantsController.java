13e9db2e471d851ce41bba9ad26ffb77fc79f843
mohammedkhalfan <mkhalfan@nyu.edu>
2026-07-03 18:15:25 -0400

    #3221: address third round of review feedback for bulk column visibility phase 2

--- miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/ConstantsController.java
+++ miso-web/src/main/java/uk/ac/bbsrc/tgac/miso/webapp/controller/ConstantsController.java
@@ -268,7 +268,7 @@ public class ConstantsController {
   private boolean showParentUsed;
   @Value("${miso.display.bulk.receipt:true}")
   private boolean showReceipt;
-  @Value("${miso.required.bulk.kitLot:true}")
+  @Value("${miso.required.kitLot:true}")
   private boolean requireKitLot;
   @Value("${miso.defaults.bulk.poolQcPassed:#{null}}")
   private Boolean defaultPoolQcPassed;
--- miso-web/src/main/webapp/scripts/bulk_pool.js
+++ miso-web/src/main/webapp/scripts/bulk_pool.js
@@ -219,7 +219,7 @@ BulkTarget.pool = (function ($) {
               ? "Ready"
               : Constants.defaultPoolQcPassed === false
                 ? "Failed"
-                : undefined,
+                : "Not Ready",
             getItemLabel: Utils.array.get("label"),
             getItemValue: Utils.array.get("value"),
           },
